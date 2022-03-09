87ff82d597b0f6795a761f8bc97a5fcd2317809f
LarryLuong <101235320+LarryLuong@users.noreply.github.com>
2022-03-08 23:27:42 -0500

    Add files via upload

---
+++ Lab 1/Larry Nguyen Lab 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Lab 1
+// CIS 199-50
+//Due: 9/5/2021
+//By: S5033
+
+// This program displays student's Grading ID,
+// hobbies, favorite book and movie to the console.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Larry_Nguyen_Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Grading ID:     S5033");
+            WriteLine("Hobbies:        Computers and Rock Climbing");
+            WriteLine("Favorite Book:  Percy Jackson");
+            WriteLine("Favorite Movie: John Wick");
+        }
+    }
+}
